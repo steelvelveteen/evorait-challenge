57330409beaf4df31aef68470871e8aeb9ef1457
JoeyVico <joeyvico@gmail.com>
2023-11-16 12:11:25 +0100

    Displaying item details under shell instead of under the list component

--- src/app/materials/material-list/material-list.component.ts
+++ src/app/materials/material-list/material-list.component.ts
@@ -2,32 +2,27 @@ import { Component, EventEmitter, Output, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataService } from '../../services/http/data.service';
 import { MaterialModel } from '../../domain/material.interface';
-import { MaterialItemDetailsComponent } from '../material-item-details/material-item-details.component';
 
 @Component({
   selector: 'app-material-list',
   standalone: true,
   templateUrl: './material-list.component.html',
   styleUrl: './material-list.component.scss',
-  imports: [CommonModule, MaterialItemDetailsComponent],
+  imports: [CommonModule],
 })
 export class MaterialListComponent {
-  @Output() materialItemSelected: EventEmitter<MaterialModel> = new EventEmitter<MaterialModel>();
-
   private dataService = inject(DataService);
-  selectedMaterialItem: MaterialModel | null = null;
-
   materials = this.dataService.materials;
 
-  selectedMaterial(material: MaterialModel): void {
-    console.log(material);
-    this.selectedMaterialItem = material;
-    this.materialItemSelected.emit();
+  @Output() materialItemSelected: EventEmitter<MaterialModel> = new EventEmitter<MaterialModel>();
+
+  selectedMaterial(materialItem: MaterialModel): void {
+    this.materialItemSelected.emit(materialItem);
   }
 
-  bookMaterial(event: Event, material: MaterialModel): void {
+  bookItem(event: Event, material: MaterialModel): void {
     event.stopPropagation();
-    console.log('click on book');
+    console.log('clicking on booking item');
     console.log(material.DescTxt);
   }
 }
--- src/app/materials/material-shell/material-shell.component.ts
+++ src/app/materials/material-shell/material-shell.component.ts
@@ -2,20 +2,25 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataService } from '../../services/http/data.service';
 import { MaterialListComponent } from '../material-list/material-list.component';
+import { MaterialItemDetailsComponent } from '../material-item-details/material-item-details.component';
+import { MaterialModel } from '../../domain/material.interface';
 
 @Component({
   selector: 'app-material-shell',
   standalone: true,
   templateUrl: './material-shell.component.html',
   styleUrl: './material-shell.component.scss',
-  imports: [CommonModule, MaterialListComponent],
+  imports: [CommonModule, MaterialListComponent, MaterialItemDetailsComponent],
 })
 export class MaterialShellComponent {
   private dataService = inject(DataService);
 
   materials = this.dataService.materials;
+  materialItem: any;
 
-  displayMaterialItemDetails(): void {
+  displayMaterialItemDetails(materialItem: MaterialModel): void {
+    this.materialItem = materialItem;
+    console.log(materialItem);
     console.log('Displaying details');
   }
 }
