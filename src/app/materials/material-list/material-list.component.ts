90761ca5d6f2e239dee8dc3e0f77e2b746a3882d
joeyOnMac <joeyvico@gmail.com>
2023-11-19 22:20:54 +0100

    Major refactoring part 2

--- src/app/materials/material-list/material-list.component.ts
+++ src/app/materials/material-list/material-list.component.ts
@@ -1,10 +1,8 @@
 import { AfterViewInit, Component, ElementRef, ViewChild, computed, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataService } from '../../services/http/data.service';
-import { Material } from '../../domain/material.interface';
 import { distinctUntilChanged, fromEvent, map } from 'rxjs';
 import { MaterialModule } from '../../material.module';
-import { Router } from '@angular/router';
 import { MaterialItemComponent } from '../material-item/material-item.component';
 
 @Component({
@@ -16,7 +14,6 @@ import { MaterialItemComponent } from '../material-item/material-item.component'
 })
 export class MaterialListComponent implements AfterViewInit {
   private dataService = inject(DataService);
-  private router = inject(Router);
 
   materials = this.dataService.materials;
   filteredMaterials = this.dataService.materials;
@@ -36,43 +33,6 @@ export class MaterialListComponent implements AfterViewInit {
       .subscribe(data => {
         this.filterMaterialsList(data);
       });
-  }
-
-  /**
-   * Emits the material selected for displaying its details
-   * @param materialItem the selected item from the list
-   */
-  selectMaterial(materialItem: Material): void {
-    this.router.navigate(['details']);
-    this.dataService.selectMaterial(materialItem);
-  }
-
-  bookMaterial(event: Event, quantity: string, material: Material): void {
-    event.stopPropagation();
-    if (!quantity) {
-      // Display some warning
-      return;
-    }
-    if (parseInt(quantity) > parseInt(material.Available)) {
-      // Display some error
-      return;
-    }
-
-    this.dataService.bookMaterial(material, quantity);
-  }
-
-  /**
-   * Prevents user from entering non-numerical values
-   * @param event the event coming from book input
-   */
-  handleBookInputChange(event: Event): void {
-    const inputValue = (event.target as HTMLInputElement).value.trim();
-    if (/^\d+$/.test(inputValue)) {
-    } else {
-      if (this.quantityInputRef) {
-        this.quantityInputRef.nativeElement.value = '';
-      }
-    }
   }
 
   private filterMaterialsList(terms: string): void {
