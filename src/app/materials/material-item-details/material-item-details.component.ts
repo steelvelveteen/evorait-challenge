9ca5e2b7bf81ee7a4572703380c2f381c8d5ae88
steelvelveteen <joeyvico@gmail.com>
2023-11-20 08:29:06 +0100

    Cleaning up files

--- src/app/materials/material-item-details/material-item-details.component.ts
+++ src/app/materials/material-item-details/material-item-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataService } from '../../services/data.service';
 import { Router } from '@angular/router';
